5784275c67f03805a25ca9a3fbb6a4b0a2324ff1
klondrag <18405064+klondrag@users.noreply.github.com>
2018-09-15 16:44:17 +0200

    added css grid

--- app/index.js
+++ app/index.js
@@ -4,3 +4,13 @@ import * as d3 from "d3";
 
 console.log(data.papers);
 console.log(data.sections);
+
+var svgSelection = d3.select("#visualization");
+
+for (var section in data.sections) {
+    svgSelection.append("circle")
+        .attr("cx", 25)
+        .attr("cy", 25)
+        .attr("r", 25)
+        .style("fill", "purple");
+}
