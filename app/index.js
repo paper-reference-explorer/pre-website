93a460aab6e78c97b5b8dacd0aca1c34871b046b
klondrag <18405064+klondrag@users.noreply.github.com>
2018-09-15 17:30:41 +0200

    added drawing of circles per section

--- app/index.js
+++ app/index.js
@@ -5,12 +5,18 @@ import * as d3 from "d3";
 console.log(data.papers);
 console.log(data.sections);
 
-var svgSelection = d3.select("#visualization");
+var rowHeight = 120;
+var nSections = data.sections.length;
+var totalHeight = rowHeight * nSections;
 
-for (var section in data.sections) {
+var svgSelection = d3.select("#visualization");
+svgSelection.attr("height", totalHeight.toString() + "px");
+
+for (var index = 0; index < nSections; index++) {
+    // data.sections[index]
     svgSelection.append("circle")
         .attr("cx", 25)
-        .attr("cy", 25)
+        .attr("cy", 25 + rowHeight * index)
         .attr("r", 25)
         .style("fill", "purple");
 }
