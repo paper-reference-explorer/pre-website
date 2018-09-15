65463c6be8fcd117e489c8b3b8dac47b65a2c783
klondrag <18405064+klondrag@users.noreply.github.com>
2018-09-16 01:22:18 +0200

    set ordering of sections by making it a list

---
+++ app/data.js
@@ -0,0 +1,84 @@
+var data = [
+  {
+    "2018": {
+      "A_my": {
+        "authors": "A et al.",
+        "title": "My first paper",
+        "date-created": "2018-09-01",
+        "referenced-n-times-global": 0,
+        "referenced-n-times-local": 0,
+        "references-keys": [
+          "D_old"
+        ]
+      },
+      "B_friendly": {
+        "authors": "B et al.",
+        "title": "Friendly paper",
+        "date-created": "2018-04-23",
+        "referenced-n-times-global": 5,
+        "referenced-n-times-local": 0,
+        "references-keys": [
+          "D_old",
+          "E_older"
+        ]
+      }
+    }
+  },
+  {
+    "2017": {}
+  },
+  {
+    "2016": {
+      "F_young": {
+        "authors": "F et al.",
+        "title": "Young paper",
+        "date-created": "2016-02-16",
+        "referenced-n-times-global": 37,
+        "referenced-n-times-local": 1,
+        "references-keys": []
+      },
+      "C_another": {
+        "authors": "C et al.",
+        "title": "Another friendly paper",
+        "date-created": "2016-12-05",
+        "referenced-n-times-global": 8,
+        "referenced-n-times-local": 0,
+        "references-keys": [
+          "F_young"
+        ]
+      }
+    }
+  },
+  {
+    "2015": {}
+  },
+  {
+    "2014": {}
+  },
+  {
+    "2013": {
+      "D_old": {
+        "authors": "D et al.",
+        "title": "Old paper",
+        "date-created": "2013-04-01",
+        "referenced-n-times-global": 236,
+        "referenced-n-times-local": 2,
+        "references-keys": []
+      }
+    }
+  },
+  {
+    "2012": {
+      "E_older": {
+        "authors": "E et al.",
+        "title": "Older paper",
+        "date-created": "2012-08-13",
+        "referenced-n-times-global": 412,
+        "referenced-n-times-local": 1,
+        "references-keys": []
+      }
+    }
+  }
+];
+
+export {data};
--- app/index.js
+++ app/index.js
@@ -1,43 +1,66 @@
 import 'styles/index.scss';
-import * as data from './data.json';
+import {data} from './data.js';
 import * as d3 from "d3";
 
-console.log(data.papers);
-console.log(data.sections);
+console.log(data);
 
-var nSections = data.sections.length;
+function len(o) {
+    var count = 0;
+    for (var key in o) {
+        if (o.hasOwnProperty(key)) {
+            count++;
+        }
+    }
+    return count
+}
 
+var nSections = data.length;
 var svgSelection = d3.select("#visualization");
 
-for (var index = 0; index < nSections; index++) {
-    var sectionHeight = 100.0 / nSections;
-    var offset = sectionHeight * index;
+for (var sectionIndex = 0; sectionIndex < nSections; sectionIndex++) {
+    var sectionWrapper = data[sectionIndex];
+    for (var sectionKey in sectionWrapper) {
+        if (sectionWrapper.hasOwnProperty(sectionKey)) {
+            var section = sectionWrapper[sectionKey];
+            var sectionHeight = 100.0 / nSections;
+            var offset = sectionHeight * sectionIndex;
 
-    svgSelection.append("line")
-        .attr("x1", 0)
-        .attr("x2", 100)
-        .attr("y1", offset + 0.25)
-        .attr("y2", offset + 0.25);
+            svgSelection.append("line")
+                .attr("x1", 0)
+                .attr("x2", 100)
+                .attr("y1", offset + 0.25)
+                .attr("y2", offset + 0.25);
 
-    svgSelection.append("text")
-        .attr("class", "section")
-        .attr("x", 0)
-        .attr("y", offset + 5)
-        .text(data.sections[index]);
+            svgSelection.append("text")
+                .attr("class", "section")
+                .attr("x", 0)
+                .attr("y", offset + 5)
+                .text(sectionKey);
 
-    var g = svgSelection.append("g");
+            var nPapersInSection = len(section);
+            var paperIndex = 0;
+            for (var paperKey in section) {
+                if (section.hasOwnProperty(paperKey)) {
+                    var paper = section[paperKey];
 
-    g.append("circle")
-        .attr("cx", "50%")
-        .attr("cy", offset + sectionHeight / 2)
-        .attr("r", 5)
-        .style("fill", "purple");
+                    var g = svgSelection.append("g");
+                    g.append("circle")
+                        .attr("cx", "50%")
+                        .attr("cy", offset + sectionHeight / 2)
+                        .attr("r", 5)
+                        .style("fill", "purple");
 
-    g.append("text")
-        .attr("class", "paper-label")
-        .attr("x", "50%")
-        .attr("y", offset + sectionHeight / 2)
-        .attr("text-anchor", "middle")
-        .attr("dominant-baseline", "middle")
-        .text(data.sections[index]);
+                    g.append("text")
+                        .attr("class", "paper-label")
+                        .attr("x", "50%")
+                        .attr("y", offset + sectionHeight / 2)
+                        .attr("text-anchor", "middle")
+                        .attr("dominant-baseline", "middle")
+                        .text(paperKey);
+
+                    paperIndex++;
+                }
+            }
+        }
+    }
 }
