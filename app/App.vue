06d4ffbcc4485fbd97bff5ec220be57c36ccc9b6
klondrag <18405064+klondrag@users.noreply.github.com>
2018-09-16 18:03:22 +0200

    implemented added papers card

--- app/App.vue
+++ app/App.vue
@@ -26,7 +26,25 @@
                     </md-card-expand>
                 </md-card>
             </div>
-            <div class="user"> 
+            <div class="user">
+                <md-card>
+                    <md-card-header>
+                        <div class="md-title">Added papers</div>
+                    </md-card-header>
+                    <md-card-content>
+                        <md-list class="md-double-line">
+                            <md-list-item>
+                                <div class="md-list-item-text">
+                                    <span>My paper</span>
+                                    <span>A et. al.</span>
+                                </div>
+                                <md-button class="md-icon-button md-list-action">
+                                    <md-icon>delete</md-icon>
+                                </md-button>
+                            </md-list-item>
+                        </md-list>
+                    </md-card-content>
+                </md-card>
             </div>
         </div>
     </div>
