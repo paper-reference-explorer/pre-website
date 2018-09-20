3c11c7260578b2d7aceb62e76ecd4840816a9d49
klondrag <18405064+klondrag@users.noreply.github.com>
2018-09-20 16:29:05 +0200

    implemented expandable behavior for user card

--- app/components/User.vue
+++ app/components/User.vue
@@ -1,26 +1,42 @@
 <template>
-    <md-card>
-        <md-card-header>
-            <div class="md-title">Added papers</div>
-        </md-card-header>
-        <md-card-content>
-            <md-list class="md-double-line">
-                <md-list-item>
-                    <div class="md-list-item-text">
-                        <span>My paper</span>
-                        <p>A et. al.</p>
-                    </div>
-                    <md-button class="md-icon-button md-list-action">
-                        <md-icon>delete</md-icon>
+    <md-card ref="card">
+        <md-card-expand>
+            <md-card-actions md-alignment="space-between" class="fake-card-title-wrapper">
+                <md-card-header class="fake-card-title-header">
+                    <div class="md-title">Added papers</div>
+                </md-card-header>
+
+                <md-card-expand-trigger>
+                    <md-button class="md-icon-button">
+                        <md-icon>keyboard_arrow_down</md-icon>
                     </md-button>
-                </md-list-item>
-            </md-list>
-        </md-card-content>
+                </md-card-expand-trigger>
+            </md-card-actions>
+
+            <md-card-expand-content>
+                <md-card-content>
+                    <md-list class="md-double-line">
+                        <md-list-item>
+                            <div class="md-list-item-text">
+                                <span>My paper</span>
+                                <p>A et. al.</p>
+                            </div>
+                            <md-button class="md-icon-button md-list-action">
+                                <md-icon>delete</md-icon>
+                            </md-button>
+                        </md-list-item>
+                    </md-list>
+                </md-card-content>
+            </md-card-expand-content>
+        </md-card-expand>
     </md-card>
 </template>
 
 <script>
     export default {
-        name: 'User'
+        name: "User",
+        mounted() {
+            this.$refs.card.MdCard.expand = true;
+        }
     };
 </script>
