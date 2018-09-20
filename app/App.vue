7ad11dd2fbfce2d5ac4825bf5ba9d603f7348692
klondrag <18405064+klondrag@users.noreply.github.com>
2018-09-20 15:10:38 +0200

    adjusted margins for information box title

--- app/App.vue
+++ app/App.vue
@@ -9,9 +9,12 @@
             </div>
             <div class="info">
                 <md-card>
+
                     <md-card-expand>
-                        <md-card-actions md-alignment="space-between">
-                            <span class="md-title">Information</span>
+                        <md-card-actions md-alignment="space-between" class="fake-card-title-wrapper">
+                            <md-card-header class="fake-card-title-header">
+                                <div class="md-title">Information</div>
+                            </md-card-header>
 
                             <md-card-expand-trigger>
                                 <md-button class="md-icon-button">
@@ -28,6 +31,11 @@
                             </md-card-content>
                         </md-card-expand-content>
                     </md-card-expand>
+                    <md-card-content>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea,
+                        nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas,
+                        saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                    </md-card-content>
                 </md-card>
             </div>
             <div class="user">
