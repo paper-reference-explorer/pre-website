3e78451281901f2d2a27699c500932698438a0d9
klondrag <18405064+klondrag@users.noreply.github.com>
2018-09-20 16:21:19 +0200

    added theme css

--- app/index.js
+++ app/index.js
@@ -4,8 +4,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 
 Vue.use(VueMaterial);
+
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
