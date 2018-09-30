e7ff321476479545f3bb2ec8e785be49f15742a6
klondrag <18405064+klondrag@users.noreply.github.com>
2018-10-01 00:17:43 +0200

    changed toolbar title

--- app/App.vue
+++ app/App.vue
@@ -74,7 +74,7 @@
         >
             <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
                 <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-                <span class="hidden-sm-and-down">Google Contacts</span>
+                <span class="hidden-sm-and-down">Paper references explorer</span>
             </v-toolbar-title>
             <v-text-field
                 flat
