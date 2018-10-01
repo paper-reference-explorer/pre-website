7df9c6485bfadc8ba4e58a38f5c0791f08318e9f
klondrag <18405064+klondrag@users.noreply.github.com>
2018-10-01 23:33:11 +0200

    adopted mock data to have key and year

--- app/index.js
+++ app/index.js
@@ -13,12 +13,12 @@ var mock = new MockAdapter(axios, {delayResponse: 500});
 
 mock.onGet('/search').reply(200, {
     papers: [
-        {"authors": "A et al.", "title": "My first paper", "date-created": "2018-09-01"},
-        {"authors": "B et al.", "title": "Friendly paper", "date-created": "2018-04-23"},
-        {"authors": "C et al.", "title": "Another friendly paper", "date-created": "2016-12-05"},
-        {"authors": "D et al.", "title": "Old paper", "date-created": "2013"},
-        {"authors": "E et al.", "title": "Older paper", "date-created": "2012"},
-        {"authors": "F et al.", "title": "Young paper", "date-created": "2016-02-16"}
+        {"key": "A_my", "authors": "A et al.", "title": "My first paper", "year": "2018"},
+        {"key": "B_friendly", "authors": "B et al.", "title": "Friendly paper", "year": "2018"},
+        {"key": "C_another", "authors": "C et al.", "title": "Another friendly paper", "year": "2016"},
+        {"key": "D_old", "authors": "D et al.", "title": "Old paper", "year": "2013"},
+        {"key": "E_older", "authors": "E et al.", "title": "Older paper", "year": "2012"},
+        {"key": "F_young", "authors": "F et al.", "title": "Young paper", "year": "2016"}
     ]
 });
 
