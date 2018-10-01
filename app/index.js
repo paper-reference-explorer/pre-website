768c203e7072bc70a86b98a812a7b73fe0b56056
klondrag <18405064+klondrag@users.noreply.github.com>
2018-10-01 20:29:21 +0200

    added vuex

--- app/index.js
+++ app/index.js
@@ -4,12 +4,23 @@ import Vue from 'vue';
 import App from './components/App.vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import Vuex from 'vuex';
+import 'es6-promise/auto';
 
 Vue.use(Vuetify);
+Vue.use(Vuex);
+
+var store = new Vuex.Store({
+    state: {},
+    getters : {},
+    mutations: {},
+    actions : {}
+});
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
+    store,
     template: '<App/>',
     components: {App},
 });
