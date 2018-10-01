0196c3bc511b2df06bb757e0a2c0d587182cdd6f
klondrag <18405064+klondrag@users.noreply.github.com>
2018-10-01 02:47:13 +0200

    reworked dialog to hold dummy text

--- app/App.vue
+++ app/App.vue
@@ -77,69 +77,28 @@
 
         <v-dialog v-model="dialog" width="800px">
             <v-card>
-                <v-card-title class="grey lighten-4 py-4 title">
-                    Create contact
+                <v-card-title
+                    class="headline primary white--text"
+                    primary-title
+                >
+                    About
                 </v-card-title>
 
-                <v-container grid-list-sm class="pa-4">
-                    <v-layout row wrap>
-                        <v-flex xs12 align-center justify-space-between>
-                            <v-layout align-center>
-                                <v-avatar size="40px" class="mr-3">
-                                    <img
-                                        src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                                        alt=""
-                                    >
-                                </v-avatar>
-                                <v-text-field
-                                    placeholder="Name"
-                                ></v-text-field>
-                            </v-layout>
-                        </v-flex>
-
-                        <v-flex xs6>
-                            <v-text-field
-                                prepend-icon="business"
-                                placeholder="Company"
-                            ></v-text-field>
-                        </v-flex>
-
-                        <v-flex xs6>
-                            <v-text-field
-                                placeholder="Job title"
-                            ></v-text-field>
-                        </v-flex>
-
-                        <v-flex xs12>
-                            <v-text-field
-                                prepend-icon="mail"
-                                placeholder="Email"
-                            ></v-text-field>
-                        </v-flex>
-
-                        <v-flex xs12>
-                            <v-text-field
-                                type="tel"
-                                prepend-icon="phone"
-                                placeholder="(000) 000 - 0000"
-                                mask="phone"
-                            ></v-text-field>
-                        </v-flex>
-
-                        <v-flex xs12>
-                            <v-text-field
-                                prepend-icon="notes"
-                                placeholder="Notes"
-                            ></v-text-field>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
+                <v-card-text>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea,
+                    nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas,
+                    saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                </v-card-text>
 
                 <v-card-actions>
-                    <v-btn flat color="primary">More</v-btn>
                     <v-spacer></v-spacer>
-                    <v-btn flat color="primary" @click="dialog = false">Cancel</v-btn>
-                    <v-btn flat @click="dialog = false">Save</v-btn>
+                    <v-btn
+                        color="primary"
+                        flat
+                        @click="dialog = false"
+                    >
+                        Close
+                    </v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
