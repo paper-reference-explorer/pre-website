64bd2841191b43213cfe483885e0a8a17802a174
klondrag <18405064+klondrag@users.noreply.github.com>
2018-10-01 02:03:32 +0200

    added clearable to search box

--- app/App.vue
+++ app/App.vue
@@ -50,6 +50,7 @@
                 flat
                 solo-inverted
                 hide-details
+                clearable
                 prepend-inner-icon="search"
                 label="Search"
                 class="hidden-sm-and-down"
