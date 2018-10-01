5143f848bd0e79c1101d878e1beb871ae4271b6a
klondrag <18405064+klondrag@users.noreply.github.com>
2018-10-01 23:34:37 +0200

    implemented search items with mock data

--- app/components/Search.vue
+++ app/components/Search.vue
@@ -7,12 +7,48 @@
         prepend-inner-icon="search"
         label="Search"
         class="hidden-sm-and-down"
+        :loading="loading"
+        :items="items"
+        :search-input.sync="search"
+        v-model="select"
     ></v-autocomplete>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: "Search"
+        name: "Search",
+        data: () => ({
+            loading: false,
+            items: [],
+            search: null,
+            select: null,
+        }),
+        watch: {
+            search(val) {
+                val && val !== this.select && this.querySelections(val);
+            }
+        },
+        methods: {
+            querySelections(query) {
+                this.loading = true;
+                axios.get('/search', {params: {keyword: query}})
+                    .then((response) => {
+                        this.items = response.data.papers
+                            .map(p => p.year + " - " + p.authors + " " + p.title)
+                            .filter(e => {
+                                return (e || '').toLowerCase().indexOf((query || '').toLowerCase()) > -1
+                            });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
+                    .then(() => {
+                        this.loading = false;
+                    });
+            }
+        }
     }
 </script>
 
