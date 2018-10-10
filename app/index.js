2d18ab39116cae3851362718aa8749dd0f2551c1
klausondrag <18405064+klausondrag@users.noreply.github.com>
2018-10-10 02:50:52 +0200

    updated search to use container

--- app/index.js
+++ app/index.js
@@ -10,10 +10,10 @@ import MockAdapter from 'axios-mock-adapter';
 import {graphPapersExample, allPapersMock} from './example-data.js';
 
 Vue.use(Vuetify);
-var mock = new MockAdapter(axios, {delayResponse: 1000});
+// var mock = new MockAdapter(axios, {delayResponse: 1000});
 
-mock.onGet('/search').reply(200, {papers: allPapersMock});
-mock.onGet('/references').reply(200, {papers: graphPapersExample});
+// mock.onGet('/search').reply(200, {papers: allPapersMock});
+// mock.onGet('/references').reply(200, {papers: graphPapersExample});
 
 /* eslint-disable no-new */
 new Vue({
