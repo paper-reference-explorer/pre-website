d68a8259bcaa2df4f28dd0aebb71a9358284bc45
klausondrag <18405064+klausondrag@users.noreply.github.com>
2019-03-09 05:11:36 +0100

    convert main.ts

--- new/src/main.ts
+++ new/src/main.ts
@@ -1,9 +1,26 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vuetify from 'vuetify';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import exampleData from './example-data';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#9C27B0',
+    secondary: '#00f00f',
+    accent: '#FF9800',
+  },
+});
+
+new MockAdapter(axios, { delayResponse: 1000 })
+  .onGet('/references').reply(200, { papers: exampleData.graphPapersExample })
+  .onAny()
+  .passThrough();
 
 new Vue({
   router,
